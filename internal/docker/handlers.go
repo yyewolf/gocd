23ddbd7ab7a5d9ef6514e9398cf132017bce91ea
Yewolf <yyewolf@gmail.com>
2023-11-24 21:32:09 +0100

    fix(cd): more logs

--- internal/docker/handlers.go
+++ internal/docker/handlers.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/filters"
 	"github.com/sirupsen/logrus"
 )
 
@@ -79,13 +80,6 @@ func UpdateContainers(token string) error {
 		for _, c := range list {
 			logrus.Infof("Updating container %s", c.Inspect.Name)
 
-			// Get current image ID from container
-			i, _, err := cli.ImageInspectWithRaw(context.Background(), c.Inspect.Image)
-			if err != nil {
-				logrus.Error("Failed to update container at image inspect: ", err)
-				continue
-			}
-
 			// Pull image, delete container, create new container
 			out, err := cli.ImagePull(context.Background(), c.Inspect.Config.Image, types.ImagePullOptions{})
 			if err != nil {
@@ -102,12 +96,20 @@ func UpdateContainers(token string) error {
 
 			outStr := string(buf[:])
 
-			err = cli.ContainerStop(context.Background(), c.ID, container.StopOptions{})
+			logrus.Debug("Image pull output: ", outStr)
+
+			logrus.Debug("Stopping container")
+			err = cli.ContainerStop(context.Background(), c.ID, container.StopOptions{
+				Signal: "SIGKILL",
+			})
 			if err != nil {
 				logrus.Error("Failed to update container at container stop: ", err)
 			}
 
-			err = cli.ContainerRemove(context.Background(), c.ID, types.ContainerRemoveOptions{})
+			logrus.Debug("Removing container")
+			err = cli.ContainerRemove(context.Background(), c.ID, types.ContainerRemoveOptions{
+				Force: true,
+			})
 			if err != nil {
 				logrus.Error("Failed to update container at container remove: ", err)
 				logrus.Error("Attempting to start container anyway")
@@ -122,12 +124,15 @@ func UpdateContainers(token string) error {
 				}
 			}
 
+			logrus.Debug("Creating new container")
+			// Create new container
 			resp, err := cli.ContainerCreate(context.Background(), c.Inspect.Config, c.Inspect.HostConfig, nil, nil, c.Inspect.Name)
 			if err != nil {
 				logrus.Error("Failed to update container at container create: ", err)
 				continue
 			}
 
+			logrus.Debug("Connecting new container to network")
 			// Put networking config into new container
 			for k, v := range nw {
 				err = cli.NetworkConnect(context.Background(), k, resp.ID, v)
@@ -136,6 +141,7 @@ func UpdateContainers(token string) error {
 				}
 			}
 
+			logrus.Debug("Starting new container")
 			err = cli.ContainerStart(context.Background(), resp.ID, types.ContainerStartOptions{})
 			if err != nil {
 				logrus.Error("Failed to update container at container start: ", err)
@@ -143,13 +149,14 @@ func UpdateContainers(token string) error {
 			}
 
 			if !strings.Contains(outStr, "Image is up to date") {
+				logrus.Debug("Removing old image")
 				// Remove old image
-				_, err = cli.ImageRemove(context.Background(), i.ID, types.ImageRemoveOptions{})
+				_, err = cli.ImagesPrune(context.Background(), filters.NewArgs())
 				if err != nil {
 					logrus.Error("Failed to remove old image: ", err)
 				}
 			}
-
+			logrus.Debug("Finished updating container")
 		}
 		containers[token] = listCopy
 	}()
--- test/push.sh
+++ test/push.sh
@@ -3,3 +3,6 @@ cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 5 | head -n 1 > test
 
 docker build -t localhost:5000/test:latest .
 docker push localhost:5000/test:latest
+
+# remove image from local
+docker rmi localhost:5000/test:latest
